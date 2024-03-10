34cb56d28536ac7019de7f3bc756b567b56e5fdd
ctaphal <67525176+ctaphal@users.noreply.github.com>
2024-03-10 13:08:42 -0400

    added router and store

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 
 <template>
-  <h1 class="bg-blue-500">App.vue</h1>
-  <h1 className="text-3xl font-bold underline">"Hello world!"</h1>
+  <router-view />
 </template>
 
 <script setup>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <pre>{{ meals }}</pre>
+</template>
+
+
+<script setup>
+import {computed} from 'vue';
+import store from '../store';
+
+const meals = computed(() => store.state.meals)
+
+</script>
